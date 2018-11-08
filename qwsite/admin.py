3eb86e904046d92a72f5fb3c7d31f6480d829b8f
Rafael Romero <quitiweb@gmail.com>
2018-11-08 19:31:35 +0100

    CV Update
    I've created the CV content.
    The Education view:
    - Date format
    - The margin between each other
    The Hobbies are now Training/Continuing Education:
    - I've created the new model
    - I've updated the View and the icon
    The Contact has been renewed as well
    The Experience Fom Date and To Date
    The Experience Remote option
    
    I've added an "else" option if there are not any CV actives

--- qwsite/admin.py
+++ qwsite/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Hobbies)
+admin.site.register(models.Training)
 admin.site.register(models.Education)
 admin.site.register(models.Experience)
 admin.site.register(models.Skills)
--- qwsite/models.py
+++ qwsite/models.py
@@ -1,27 +1,36 @@
 from django.db import models
+import datetime
 
+
+YEAR_CHOICES = [(r,r) for r in range(1984, datetime.date.today().year+1)]
+MONTH_CHOICES = [(r,r) for r in range(1, 13)]
 
 TITLE_MAX_LENGTH = 100
 TXT_MAX_LENGTH = 3000
 GD_MAX_LENGTH = 80
 
 
-class Hobbies(models.Model):
-    title = models.CharField(max_length = TITLE_MAX_LENGTH)
+class Training(models.Model):
+    course = models.CharField(max_length = TITLE_MAX_LENGTH)
+    company_vendor = models.CharField(max_length = TITLE_MAX_LENGTH)
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    month = models.IntegerField(('month'), choices=MONTH_CHOICES, default=datetime.datetime.now().month)
 
     class Meta:
-        verbose_name_plural = 'Hobbies'
+        verbose_name_plural = 'Training'
+        ordering = ('-year','-month',)
 
     def __str__(self):
-        return self.title
+        return self.course
 
 class Education(models.Model):
     title = models.CharField(max_length = TITLE_MAX_LENGTH)
     universityCollege = models.CharField(max_length = TITLE_MAX_LENGTH)
-    date = models.DateTimeField()
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
 
     class Meta:
         verbose_name_plural = 'Education'
+        ordering = ('-year',)
 
     def __str__(self):
         return self.title
@@ -30,14 +39,17 @@ class Experience(models.Model):
     title = models.CharField(max_length = TITLE_MAX_LENGTH)
     role = models.CharField(max_length = TITLE_MAX_LENGTH)
     place = models.CharField(max_length = TITLE_MAX_LENGTH)
-    date = models.DateTimeField()
+    remote = models.BooleanField(default=False)
+    fromDate = models.DateField(default=datetime.date.today)
+    toDate = models.DateField(default=datetime.date.today)
     description = models.TextField(max_length = TXT_MAX_LENGTH)
 
     class Meta:
         verbose_name_plural = 'Experience'
+        ordering = ('-toDate',)
 
     def __str__(self):
-        return self.title
+        return self.title + ' - ' + self.place + ' - ' + str(self.fromDate)
 
 class Skills(models.Model):
     title = models.CharField(max_length = TITLE_MAX_LENGTH)
@@ -58,7 +70,7 @@ class cv(models.Model):
     skills = models.ManyToManyField(Skills)
     experience = models.ManyToManyField(Experience)
     education = models.ManyToManyField(Education)
-    hobbies = models.ManyToManyField(Hobbies)
+    training = models.ManyToManyField(Training)
     activo = models.BooleanField(default=False)
 
     class Meta:
