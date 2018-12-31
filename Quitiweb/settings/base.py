321c7ac677e7a06a6b8df7d4b28876f93ba03093
Rafa <rafa@quitiweb.com>
2018-12-31 18:43:09 +0100

    The template folder doesn't exist in prod

--- Quitiweb/settings/base.py
+++ Quitiweb/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Quitiweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),'templates', 'Quitiweb/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
