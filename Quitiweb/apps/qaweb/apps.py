a149ff8ddcf312b44ea301e3772942d85d7622cc
Quitiweb <rafa@quitiweb.com>
2022-05-16 14:27:03 +0200

    update to django 4.0.4

--- Quitiweb/apps/qaweb/apps.py
+++ Quitiweb/apps/qaweb/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class QAWebConfig(AppConfig):
-    name = 'qaweb'
+    name = 'Quitiweb.apps.qaweb'
--- Quitiweb/settings/prod.py
+++ Quitiweb/settings/prod.py
@@ -1,14 +1,15 @@
-"""Production settings and globals"""
+"""
+    Production settings and globals
+"""
 
 from __future__ import absolute_import
-
 from os import environ
-
 from .base import *
 
 # Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
+# into your settings, but ImproperlyConfigured is an exception
 from django.core.exceptions import ImproperlyConfigured
+
 
 def get_env_setting(setting):
     """ Get the environment setting or return exception """
@@ -18,9 +19,8 @@ def get_env_setting(setting):
         error_msg = "Set the %s env variable" % setting
         raise ImproperlyConfigured(error_msg)
 
+
 DEBUG = False
 
-########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('SECRET_KEY')
-########## END SECRET CONFIGURATION
